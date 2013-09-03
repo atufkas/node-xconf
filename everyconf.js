47f7d75cc7a891dcd9f63e4823c39b27073ee03b
Matthias Lienau <matthias@mlienau.de>
2013-09-03 15:33:48 +0200

    Changed parse() to auto(). Still evaluating...

--- everyconf.js
+++ everyconf.js
@@ -1,15 +1,23 @@
 var _ = require('underscore');
 
+
 var EveryConfig = {
-
-  get: function(env) {
-
-    env = env || process.env.NODE_ENV || 'development';
-
-    var _rawconfig = require('./config')
-      , config = _.defaults(_rawconfig[ env ],_rawconfig[ '_defaults' ]);
-
-    return config;
+  
+  options: {
+    env: process.env.NODE_ENV || 'development',
+    format: 'js'
+  },
+  
+  _cachedConfig: null,
+  
+  auto: function(name) {
+    
+    name = name || 'config';
+    env = process.env.NODE_ENV || 'development';
+    file = '../../' + name + '.js';
+    
+    var _rawconfig = require(file)
+    return this._cachedConfig = _.defaults(_rawconfig[ env ],_rawconfig[ '_defaults' ]);
   }
 };
 
